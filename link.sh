79ddc4469df9888662398b4e9ce52b76d5291743
rcesecurity <rcesecurity@users.noreply.github.com>
2015-01-18 18:12:54 +0100

    Create link.sh

---
+++ link.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '[+] Assembling with Nasm ... '
+nasm -f elf32 -o $1.o $1.nasm
+
+echo '[+] Linking ...'
+ld -z execstack -o $1 $1.o
+
+echo '[+] Dumping with objdump ... '
+objdump -d $1.o|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-7 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
+
+echo '[+] Done!'
